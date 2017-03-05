0db63b53c1316925c64398ea8d1a2be93500f6f6
James Curtis <mybastion@gmail.com>
2017-03-05 00:39:32 +0000

    Add inventory view url path

--- jedisite/urls.py
+++ jedisite/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^profile/decks/$', views.user_decks, name='user_decks'),
     url(r'^delete_account/$', views.delete_account, name='delete_account'),
     url(r'^update_postdata/$', views.update_postdata, name='update_postdata'),
-    url(r'^owned_cards/$', views.get_owned_cards, name='owned_cards'),
+    url(r'^owned_cards/$', views.download_inventory, name='owned_cards'),
     url(r'^benchmarks/$', views.benchmarks, name='benchmarks'),
     url(r'^war-ranks/$', views.ranks_war, name='ranks_war'),
     url(r'^brawl-ranks/$', views.ranks_brawl, name='ranks_brawl'),
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^deckslist/$', views.deckslist, name='deckslist'),
     url(r'^accounts_list/$', views.api_account_list, name='api_account_list'),
     url(r'^auth/$', views.force_auth, name='force_auth'),
+    url(r'^inventory/$', views.user_inventory, name='user_inventory'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
